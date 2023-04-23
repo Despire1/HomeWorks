c91172b38f725d6ea6d10b128bfe30c921bf44e0
Despire1 <123311449+Despire1@users.noreply.github.com>
2023-04-23 22:26:04 +0300

    Add files via upload

---
+++ CreateX/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+  var mixer = mixitup('.directions__list');
+  $('.directions__filter-btn').on('click', function () {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+    $(this).addClass('directions__filter-btn--active')
+  })
+
+  $('.team__slider').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 4,
+    infinite: true,
+
+  })
+
+})
